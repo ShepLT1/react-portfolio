e8c28f177a38ba4ad673b1a544cf02177e0c4302
ShepLT1 <lshepherd234@gmail.com>
2020-11-25 01:35:17 -0800

    Updates

---
+++ precache-manifest.a8c9a3d32fcfe7ac4af91446f27195d0.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039c2f356cbeee9dce779ebd3bb83554",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "083d517ccea75d29ad2f",
+    "url": "/react-portfolio/static/css/2.c19df8ea.chunk.css"
+  },
+  {
+    "revision": "b28c1635809f5a8810d8",
+    "url": "/react-portfolio/static/css/main.ce65cdb8.chunk.css"
+  },
+  {
+    "revision": "083d517ccea75d29ad2f",
+    "url": "/react-portfolio/static/js/2.b021249f.chunk.js"
+  },
+  {
+    "revision": "1a1a42c150bd05496b97b265c552c789",
+    "url": "/react-portfolio/static/js/2.b021249f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b28c1635809f5a8810d8",
+    "url": "/react-portfolio/static/js/main.2ea9d5f0.chunk.js"
+  },
+  {
+    "revision": "abd8dc3e5ba9a474aea2",
+    "url": "/react-portfolio/static/js/runtime-main.fcb30b24.js"
+  },
+  {
+    "revision": "e332833ed626d07be4d57e2af03a6d9e",
+    "url": "/react-portfolio/static/media/5sherpas.e332833e.png"
+  },
+  {
+    "revision": "355fadc91fbb5fd507ec730e3eafbad9",
+    "url": "/react-portfolio/static/media/Luke-Shepherd-Resume.355fadc9.pdf"
+  },
+  {
+    "revision": "9a06a7ab35210af710332e0ddc6cf149",
+    "url": "/react-portfolio/static/media/Slack-cover-photo.9a06a7ab.jpeg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react-portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react-portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react-portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react-portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react-portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "d24584eaf160456c6ace247d37f1becd",
+    "url": "/react-portfolio/static/media/breakaway.d24584ea.png"
+  },
+  {
+    "revision": "967f74f2eb92b24f389049a4a474df46",
+    "url": "/react-portfolio/static/media/covid-tracker.967f74f2.png"
+  },
+  {
+    "revision": "2b817cfd8d6582e29002658d334e476a",
+    "url": "/react-portfolio/static/media/day-planner.2b817cfd.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react-portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react-portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react-portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react-portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react-portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react-portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react-portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react-portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react-portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "f8e2b1b36636ed8fbf0fab6d475bdcc8",
+    "url": "/react-portfolio/static/media/password-generator.f8e2b1b3.png"
+  },
+  {
+    "revision": "89118a610ff32a6fb35118b71fa6bad9",
+    "url": "/react-portfolio/static/media/weather-forecast.89118a61.png"
+  }
+]);
